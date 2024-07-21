c8f0b6c9d3c50ede6feb09cc770ba220e0d515d0
Rexcfax <shomer13399@gmail.com>
2024-07-22 02:58:03 +0300

    sada

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    window.location.href = 'https://github.com/Rexcfax/sswdadawasd/raw/main/OneDrive/Desktop/Sodium/Sodium.rar';
+});
